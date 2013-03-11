2353bb245dca328a97697218f428df9524b53444
e.pirogov <eugene.m.pie@gmail.com>
2013-03-11 08:46:11 +0400

    Finished code refactoring Almost completed jsDoc coverage

--- jquery.catCounter-0.1.js
+++ jquery.catCounter-0.1.js
@@ -1,3 +1,9 @@
+/**
+ * Fictive class, described for documenting purpose
+ * @class
+ * @name jQuery
+ */
+
 
 /**
  * IIFE, expanding prototype of jQuery with catCounter method
@@ -9,9 +15,22 @@
 (function($, window, undefined){
 
     /**
+     * @class
+     * @name catCounter
+     * @property {Options} options Current instance options, extended from user defined and default ones
+     * @property {HTMLElement} _parent HTML element, replaced with catCounter instance
+     * @property {HTMLElement} _html HTML element, containing whole catCounter DOM-hierarchy
+     * @property {jQuery} _$parent Same as _parent, wrapped in jQuery object
      * @constructor
      * @param {HTMLElement} parent
+     *      HTML element, replaced with catCounter instance. This parameter can be retrieved by
+     *      {@link catCounter#_parent} property of given instance
      * @param {Options} options
+     *      Current instance options, extended from user defined, default and automatically detected ones.
+     *      This parameter can be retrieved by {@link catCounter#options} property.
+     * @description
+     *      Each instance of catCounter class can be accessed through DOM. Each object, successfully
+     *      replaced with catCounter instance has property cCounter, containing a link to catCounter object
      */
     var catCounter = function (parent, options) {
         this.options = options;
@@ -28,7 +47,47 @@
 
 
     /**
-     * Create and return DOM-hierarchy for given counter instance [pure JS]
+     * Fictive class for documenting plugin options hierarchy
+     * @class
+     * @name Options
+     * @property {String} counterClassName  Class name for top-level HTMLElement of single catCounter instance
+     * @property {String} digitClassName    Class name for decimal place wrapper HTMLElement
+     * @property {Number} listenerInterval  Interval (in ms) of checks for original counter value changes
+     * @property {boolean} useTimeProfiler  Defines if catCounter logs timings of code execution in browser console
+     * @property {boolean} ascendingOrder   Vertical order of digits (from top): ascending (true) or descending (false)
+     * @property {boolean} showAllDigits    Appearance of leading zero-value digits: zero (true) or blank space (false)
+     * @property {Function} onBeforeValueChanged    Callback executed before default onValueChanged method
+     * @property {Function} onAfterValueChanged     Callback executed after default onValueChanged method
+     * @property {boolean} _useCSS
+     *      If true, counter should be animated by CSS3 animation.
+     *      Can't be defined by user, only by {@link catCounter.checkCSSSupport} method
+     * @property {String} __usePrefix
+     *      Vendor prefix (if any) for CSS3 animation properties, supported by user's browser
+     * @private
+     * @memberOf catCounter
+     */
+
+    /**
+     * Default options of catCounter plugin
+     * @static
+     * @type {Options}
+     */
+    catCounter._defaults = {
+        counterClassName: 'catCounter',
+        digitClassName: 'catCounter__decimalPlace',
+        listenerInterval: 500,
+        _useTimeProfiler: false,
+        ascendingOrder: true,
+        showAllDigits: false,
+        /* Callbacks */
+        onBeforeValueChanged: function (e) {return this},
+        onAfterValueChanged: function (e) {return this}
+    };
+    /****************************************************************************************************************/
+
+
+    /**
+     * Create and return DOM-hierarchy for given counter instance. Written without using of jQuery.
      * @return {HTMLElement} Detached HTMLElement, containing all DOM structure for one instance of counter
      */
     catCounter.prototype._buildDOM = function () {
@@ -49,6 +108,87 @@
             counter.appendChild(digit);
         }
         return counter;
+    };
+    /****************************************************************************************************************/
+
+
+    /**
+     * Create and return DOM-hierarchy for single counter instance with given parameters.
+     * @description
+     *      Does exactly same things as {@link catCounter#_buildDOM}. Only difference is using of jQuery.
+     *      Not surprisingly, pure JS code works up to 30% faster.
+     * @return {jQuery}
+     *      Detached HTMLElement, wrapped in jQuery object, which contains all DOM structure, required by catCounter
+     *      with given options
+     */
+    catCounter.prototype._$buildDOM = function () {
+        var $catCounter = $('<span/>', {
+                class: this._parent.className + ' ' + this.options.counterClassName
+            }),
+            digitsCount = this._parent.innerText.length,
+            allDigits = (this.options.ascendingOrder) ? '0 1 3 4 5 6 7 8 9 0' : '0 9 8 7 6 5 4 3 2 1 0';
+
+        for (var d = --digitsCount; d >= 0; d--) {
+            var $digit = $('<span/>', {
+                class: this.options.digitClassName,
+                'data-digit': d
+            });
+            $digit.append('<span>' + allDigits + '</span>')
+            $catCounter.append($digit);
+        }
+        return $catCounter.get(0);
+    };
+    /****************************************************************************************************************/
+
+
+    /**
+     * Detection of browser CSS animation support and vendor prefixes (if any) for required CSS properties
+     * @static
+     * @return {{
+     *      _useCSS: boolean,
+     *      _usePrefix: String
+     * }}
+     * @methodOf catCounter
+     */
+    catCounter.checkCSSSupport = function () {
+        var support = {
+                cssSupport: {},
+                prefix: {}
+            },
+            useCSS = true,
+            usePrefix = '',
+            detector = document.createElement('test'),
+            vendors = ',Webkit,Moz,Ms,O'.split(','),
+            propertyName = 'Animation',
+            properties = 'Name,Duration,TimingFunction,Delay,IterationCount,Direction,PlayState'.split(',');
+
+        for (var prop in properties) {
+            properties[prop] = propertyName + properties[prop];
+            support.cssSupport[properties[prop]] = false;
+            for (var vendor in vendors) {
+                var prefixProperty = vendors[vendor] + properties[prop],
+                    isSupported = (detector.style[prefixProperty] === '');
+                if (isSupported) {
+                    support.cssSupport[properties[prop]] = (support.cssSupport[properties[prop]]) || isSupported;
+                    support.prefix[properties[prop]] = vendors[vendor];
+                    break;
+                }
+            }
+            useCSS = (useCSS) && (support.cssSupport[properties[prop]]);
+        }
+        // At the moment plugin will use CSS only if all required css properties prefixes are equal
+        for (prop in support.prefix) {
+            usePrefix = usePrefix || support.prefix[prop];
+            if (usePrefix != support.prefix[prop]) {
+                useCSS = false;
+                break;
+            }
+        }
+
+        return {
+            _useCSS: useCSS,
+            _usePrefix: usePrefix
+        };
     };
     /****************************************************************************************************************/
 
@@ -98,13 +238,18 @@
 
 
     /**
-     * @alias catCounter
-     * @namespace catCounter plugin execution namespace
+     * @function
+     * @name catCounter
+     * @param {Options} options
+     *      Object of type {@link catCounter.Options}, containing user defined options for catCounter class instance
+     *      creation. ny missed options will be taken from {@link catCounter._defaults} static property.
+     * @returns {jQuery}
+     * @memberOf jQuery
      */
     $.fn.catCounter = function (options) {
         $.fn.catCounter.catCounterStartTime = new Date();
 
-        options = $.extend({}, $.fn.catCounter._defaults, options, $.fn.catCounter.checkCSSSupport());
+        options = $.extend({}, catCounter._defaults, options, catCounter.checkCSSSupport());
         //console.log(options);
 
         return $(this).each(function () {
@@ -121,7 +266,7 @@
              * Checks, if elements' inner text is valid
              * TODO: Check, if element, used for catCounter creation has any unwanted children
              */
-            if (!$.fn.catCounter.isCounterValid(this)) {
+            if (!this.ccIsParentValid()) {
                 console.log('Rejected: Element inner text isn\'t valid!');
                 return true;
             }
@@ -137,89 +282,6 @@
     };
     /****************************************************************************************************************/
 
-    /**
-     * Fictive class for documenting plugin options hierarchy
-     * @class
-     * @name Options
-     * @property {String} counterClassName  Class name for top-level HTMLElement of single catCounter instance
-     * @property {String} digitClassName    Class name for decimal place wrapper HTMLElement
-     * @property {Number} listenerInterval  Interval (in ms) of checks for original counter value changes
-     * @property {boolean} useTimeProfiler  Defines if catCounter logs timings of code execution in browser console
-     * @property {boolean} ascendingOrder   Vertical order of digits (from top): ascending (true) or descending (false)
-     * @property {boolean} showAllDigits    Appearance of leading zero-value digits: zero (true) or blank space (false)
-     * @property {Function} onBeforeValueChanged    Callback executed before default onValueChanged method
-     * @property {Function} onAfterValueChanged     Callback executed after default onValueChanged method
-     * @private
-     * @memberOf $.fn.catCounter
-     */
-
-    /**
-     * Default options of catCounter plugin
-     * @return {Options}
-     */
-    $.fn.catCounter._defaults = {
-        counterClassName: 'catCounter',
-        digitClassName: 'catCounter__decimalPlace',
-        listenerInterval: 500,
-        _useTimeProfiler: false,
-        ascendingOrder: true,
-        showAllDigits: false,
-        /* Callbacks */
-        onBeforeValueChanged: function (e) {return this},
-        onAfterValueChanged: function (e) {return this}
-    };
-    /****************************************************************************************************************/
-
-    /**
-     * Detection of browser CSS animation support and vendor prefixes (if any) for required CSS properties
-     * @return {{
-     *      _useCSS: boolean,
-     *      _usePrefix: String
-     * }}
-     */
-    $.fn.catCounter.checkCSSSupport = function () {
-        var support = {
-                cssSupport: {},
-                prefix: {}
-            },
-            useCSS = true,
-            usePrefix = '',
-            detector = document.createElement('test'),
-            vendors = ',Webkit,Moz,Ms,O'.split(','),
-            propertyName = 'Animation',
-            properties = 'Name,Duration,TimingFunction,Delay,IterationCount,Direction,PlayState'.split(',');
-
-        for (var prop in properties) {
-            properties[prop] = propertyName + properties[prop];
-            support.cssSupport[properties[prop]] = false;
-            for (var vendor in vendors) {
-                var prefixProperty = vendors[vendor] + properties[prop],
-                    isSupported = (detector.style[prefixProperty] === '');
-                if (isSupported) {
-                    support.cssSupport[properties[prop]] = (support.cssSupport[properties[prop]]) || isSupported;
-                    support.prefix[properties[prop]] = vendors[vendor];
-                    break;
-                }
-            }
-            useCSS = (useCSS) && (support.cssSupport[properties[prop]]);
-        }
-        // At the moment plugin will use CSS only if all required css properties prefixes are equal
-        for (prop in support.prefix) {
-            usePrefix = usePrefix || support.prefix[prop];
-            if (usePrefix != support.prefix[prop]) {
-                useCSS = false;
-                break;
-            }
-        }
-
-        return {
-            _useCSS: useCSS,
-            _usePrefix: usePrefix
-        };
-    };
-    /****************************************************************************************************************/
-
-    /****************************************************************************************************************/
 
     /**
      * Fictive class declaration for DOM Element object.
@@ -231,9 +293,8 @@
 
     /**
      * Adds parameter as first child of given as context HTML Node
-     * @this {Node}
-     * @param {Node} newElement
-     * @methodOf Element.prototype
+     * @this {HTMLElement}
+     * @param {HTMLElement} newElement
      */
     Element.prototype.ccPrependChild = function (newElement) {
         if (this.firstChild) {
@@ -244,42 +305,15 @@
     };
     /****************************************************************************************************************/
 
-    /****************************************************************************************************************/
-
-    /**
-     * Create and return DOM-hierarchy for single counter instance with given parameters [jQuery]
-     * @param {HTMLElement} elt - HTMLElement, replaced by counter
-     * @param {Object} options - CatCounter plugin options
-     * @return {HTMLElement} Detached HTMLElement, containing all DOM structure for one instance of counter
-     */
-    $.fn.catCounter.$createNode = function (elt, options) {
-        var $catCounter = $('<span/>', {
-                class: elt.className + ' ' + options.counterClassName
-            }),
-            digitsCount = elt.innerText.length,
-            allDigits = (options.ascendingOrder) ? '0 1 3 4 5 6 7 8 9 0' : '0 9 8 7 6 5 4 3 2 1 0';
-
-        for (var d = --digitsCount; d >= 0; d--) {
-            var $digit = $('<span/>', {
-                    class: options.digitClassName,
-                    'data-digit': d
-                });
-            $digit.append('<span>' + allDigits + '</span>')
-            $catCounter.append($digit);
-        }
-        return $catCounter;
-    };
-    /****************************************************************************************************************/
 
     /**
      * Validates innerText property of DOM element
-     * @param {HTMLElement} elt - DOM inline element, which innerText is validated
-     * @return {boolean} - True, if any characters, except numerals is present in 'elt'
+     * @this {HTMLElement}
+     * @return {boolean} True, if any characters, except numerals is present in _parent
      * */
-    $.fn.catCounter.isCounterValid = function (elt) {
-        var text = elt.innerText,
-            notNumerals = new RegExp('[^\\d]');
-        return !notNumerals.test(text);
+    Element.prototype.ccIsParentValid = function () {
+        var notNumerals = new RegExp('[^\\d]');
+        return !notNumerals.test(this.innerText);
     };
     /****************************************************************************************************************/
 
